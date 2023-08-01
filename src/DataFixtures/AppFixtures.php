84159293af0654a374d8beaa5bbc131669c708b5
AsmaAbaa <asma.abaragh@gmail.com>
2023-08-01 10:23:55 +0200

    mettre en place les fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Note;
+use App\Entity\User;
+use Faker\Factory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'un générateur de données Faker en français
+        $faker = Factory::create('fr_FR');
+
+        // Création d'un utilisateur de test
+        $user = new User();
+        $user->setEmail('hello@codeshare.fr')
+        ->setUsername('codeshare')
+        ->setPassword('$2y$13$4UbZtgjJ2J0JSmY45CZs4uGbUbckq1R.N64JltRbz7JTVpuo3YJzi') // mdp = admin
+        ->setRoles(["ROLE_ADMIN"])
+        ->setIsVerified(true)
+        ;
+
+        // Enregistrement de l'utilisateur en base de données
+        $manager->persist($user);
+
+        // Boucle pour créer 200 notes de test
+        for ($i=0; $i < 200; $i++) {
+            $note = new Note();
+            $note->setTitle($faker->word(2))
+            ->setContent($faker->text(200))
+            ->setUser($user)
+            ->setCreatedAt($faker->dateTimeBetween('-7 months'))
+            ->setIsPublished(true)
+            ;
+
+            $manager->persist($notes);
+        }
+
+        $manager->flush();
+    }
+}
