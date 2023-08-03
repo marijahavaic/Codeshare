606552e41bf3251c0f36f3d293bab0334b77d2bf
Marija Havaic <marija.havaic@gmail.com>
2023-08-03 10:02:53 +0200

    Fix add note-ajouter __toString en NoteController

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,6 +64,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->notes= new ArrayCollection();
     }
 
+     public function __toString()
+    {
+        return $this->username;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
